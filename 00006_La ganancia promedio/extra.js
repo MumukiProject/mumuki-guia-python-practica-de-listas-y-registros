bc88c5f49810e30b69c7052c007e6b3b27191705
gustavo@mumuki.org <gustavo@mumuki.org>
2019-03-15 19:58:02 +0000

    Mumuki Export on 2019-03-15 19:58:02 +0000

--- 00006_La ganancia promedio/extra.js
+++ 00006_La ganancia promedio/extra.js
@@ -2,4 +2,10 @@ function longitud(lista) {
   return lista.length
 }
 
-/*...solution[4]...*/
+function gananciaTotal(balancesDeUnPeriodo) {
+  let sumatoria = 0;
+  for (let balance of balancesDeUnPeriodo) {
+    sumatoria = sumatoria + balance.ganancia;
+  }
+  return sumatoria;
+}
