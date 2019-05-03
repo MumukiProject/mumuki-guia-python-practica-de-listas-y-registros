a1e51ee730d5c5d2c3d26613925373e23411b03c
gustavo@mumuki.org <gustavo@mumuki.org>
2019-05-03 20:36:42 +0000

    Mumuki Export on 2019-05-03 20:36:42 +0000

--- 00003_Todas las ganancias, la ganancia/test.py
+++ 00003_Todas las ganancias, la ganancia/test.py
@@ -1,7 +1,7 @@
 
   def test_ganancia_total_4_debe_sumar_las_ganancias_de_4_balances(self):
-    self.assertEqual(ganancia_total_4([{ "mes": "enero", "ganancia": 2 }, { "mes": "febrero", "ganancia": 10 }, { "mes": "marzo", "ganancia": 20 }, { "mes": "abril", "ganancia": 0 }]), 32)
+    self.assertEqual(ganancia_total_4([{ "mes": "enero", "ganancia": 2 }, { "mes": "febrero", "ganancia": 10 }, { "mes": "marzo", "ganancia": 20 }, { "mes": "abril", "ganancia": 2 }]), 34)
   
   def test_ganancia_total_4_debe_funcionar_con_numeros_negativos(self):
-    self.assertEqual(ganancia_total_4([{ "mes": "enero", "ganancia": 2 }, { "mes": "febrero", "ganancia": 10 }, { "mes": "marzo", "ganancia": -20 }, { "mes": "abril", "ganancia": 0 }]), -8)
+    self.assertEqual(ganancia_total_4([{ "mes": "enero", "ganancia": 2 }, { "mes": "febrero", "ganancia": 10 }, { "mes": "marzo", "ganancia": -20 }, { "mes": "abril", "ganancia": 2 }]), -6)
   
